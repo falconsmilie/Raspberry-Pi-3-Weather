b3815e7ef52d6035e852a78e643350bdacdcb477
falconsmilie <shane@rededitor.net>
2016-12-30 23:06:41 +0100

    A controller of sorts to test the different classes.

---
+++ weather.py
@@ -0,0 +1,32 @@
+import sys
+from weatherCache import *
+from weatherConfig import *
+from weatherRequest import *
+
+# Testing Controller
+try:
+    print('Starting ... ')
+    
+    # Config
+    weather_config = WeatherConfig()
+    print('WeatherConfig: Instantiated')
+    
+    weather_config.set_config()
+    print('WeatherConfig: Config Set')
+    
+    wconfig = weather_config.get_config_all()
+    print('WeatherConfig: Got Config')
+
+    if wconfig == None:
+        raise Exception('No config can be found.')
+
+    # Caching
+    open_weather_cache = WeatherCache()
+    weather = open_weather_cache.check_cache(wconfig['location'])
+
+    if weather == None:
+        weather_request = WeatherRequest()
+        weather_request.set_location(wconfig['location'])
+        
+except Exception as e:
+    print(e)
