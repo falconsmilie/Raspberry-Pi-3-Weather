d3a3a4d105e6b3446ab3d005f525b6b69c427193
falconsmilie <shane@rededitor.net>
2017-01-01 23:20:56 +0100

    Added support for Language in Request class

--- models/weatherRequest.py
+++ models/weatherRequest.py
@@ -10,6 +10,7 @@ class WeatherRequest(object):
         self.__location = None
         self.__api_key = None
         self.__units = None
+        self.__lang = 'en'
 
     def set_params(self, params):
         """ Set local configuration values """
@@ -18,6 +19,7 @@ class WeatherRequest(object):
             self.set_location(params['location'])
             self.set_api_key(params['api_key'])
             self.set_units(params['units'])
+            self.set_lang(params['lang'])
 
         except KeyError as e:
             raise Exception(repr('Not a valid config key: ' + str(e)))
@@ -27,18 +29,23 @@ class WeatherRequest(object):
     def set_location(self, location):
         """ Set the location we want to query """
         self.__location = location
-
         return None
 
     def set_api_key(self, api_key):
         """ Set the client's API key for accessing OpenWeatherMaps """
         self.__api_key = api_key
-
         return None
 
     def set_units(self, units):
         """ Set the unit weather is returned in """
         self.__units = units
+        return None
+
+    def set_lang(self, lang):
+        """ Set the Langauge Description is returned in """
+        if lang:
+            self.__lang = lang
+        return None
 
     def get_weather(self):
         """ Send the request """
@@ -46,6 +53,7 @@ class WeatherRequest(object):
         payload = {
             'id': self.__location,
             'units': self.__units,
+            'lang': self.__lang,
             'APPID': self.__api_key
         }
 
