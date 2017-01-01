f2ef24ef66ad9cbac37ef81cdee8431813ce78fd
falconsmilie <shane@rededitor.net>
2017-01-01 23:11:43 +0100

    Cleaned controller

--- weather.py
+++ weather.py
@@ -4,17 +4,10 @@ from models.weatherRequest import *
 
 # Testing Controller
 try:
-    print('Starting ... ')
-
     # Config
     weather_config = WeatherConfig()
-    print('WeatherConfig: Instantiated')
-
     weather_config.set_config()
-    print('WeatherConfig: Config Set')
-
     wconfig = weather_config.get_config_all()
-    print('WeatherConfig: Got Config')
 
     if wconfig is None:
         raise Exception('No config can be found.')
@@ -24,7 +17,6 @@ try:
     # Caching
     weather_cache = WeatherCache()
     weather_cache.clean_cache()
-    print('WeatherCache: Cache cleaned')
 
     # Get Weather
     weather = weather_cache.check_cache(location)
@@ -33,10 +25,8 @@ try:
         weather_request = WeatherRequest()
         weather_request.set_params(wconfig)
         weather = weather_request.get_weather()
-        print('WeatherRequest: Request Weather')
 
         weather_cache.set_cache(weather, location)
-        print('WeatherCache: Cached Weather')
 
     print(weather)
 
