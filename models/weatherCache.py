6c39c1bb6f1d68070e785e20ac9f5ce8ac70d605
falconsmilie <shane@rededitor.net>
2017-01-02 16:18:27 +0100

    Added support for cache sub-folders

--- models/weatherCache.py
+++ models/weatherCache.py
@@ -7,31 +7,33 @@ class WeatherCache(object):
 
     def __init__(self):
         self.__path_to_cache = 'cache/'
+        self.__cache_folders = ['weather', 'forecast5', 'forecast16']
 
     def clean_cache(self, cache_time=10):
-        """ Removes cache files older than 10 minutes """
+        """ Removes cache files older than '10' minutes """
 
-        files = []
-        for file in listdir(self.__path_to_cache):
-            if file.endswith('.json'):
-                files.append(file)
+        for cache_folder in self.__cache_folders:
+            files = []
+            for file in listdir(self.__path_to_cache):
+                if file.endswith('.json'):
+                    files.append(file)
 
-        for file in files:
-            file_timestamp = '{:.10}'.format(file)
-            timestamp = time.time()
+            for file in files:
+                file_timestamp = '{:.10}'.format(file)
+                timestamp = time.time()
 
-            if (timestamp - float(file_timestamp)) / 60 > cache_time:
-                remove(self.__path_to_cache + file)
+                if (timestamp - float(file_timestamp)) / 60 > cache_time:
+                    remove(self.__path_to_cache + file)
 
         return None
 
-    def check_cache(self, filename):
+    def check_cache(self, filename, request_type):
         """ Check for existence of a cache file based on city ID """
 
         filename = '{}'.format(filename)
         
         files = glob.glob(
-            self.__path_to_cache + '*[0-9]-' + filename + '.json'
+            self.__path_to_cache + request_type + '/*[0-9]-' + filename + '.json'
         )
 
         if files:
@@ -55,7 +57,7 @@ class WeatherCache(object):
 
         return weather
 
-    def set_cache(self, weather, location):
+    def set_cache(self, weather, location, request_type):
         """ Add the weather data to the cache """
 
         weather = '{}'.format(weather)
@@ -64,7 +66,11 @@ class WeatherCache(object):
         # Remove micro seconds from timestamp
         cache_time = '{:.10}'.format(cache_time)
 
-        file_name = self.__path_to_cache + cache_time + '-' + location + '.json'
+        file_name = path.join(
+            self.__path_to_cache,
+            request_type,
+            cache_time + '-' + location + '.json'
+        )
 
         cache_file = open(file_name, 'w+')
         cache_file.write(weather)
