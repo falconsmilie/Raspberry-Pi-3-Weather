b903074ccf0a49399741289cce8cce16e654bd29
falconsmilie <shane@rededitor.net>
2017-01-02 15:07:29 +0100

    Added support for 16 day forecast

--- models/weatherRequest.py
+++ models/weatherRequest.py
@@ -12,6 +12,7 @@ class WeatherRequest(object):
         self.__api_key = None
         self.__units = None
         self.__lang = None
+        self.__forecast16_count = None
 
     def set_params(self, params):
         """ Set local configuration values """
@@ -23,7 +24,12 @@ class WeatherRequest(object):
             self.set_units(params['units'])
             self.set_lang(params['lang'])
 
+            # Specific to Forecast16
+            if self.__request_type is 'forecast16':
+                self.set_forecast16_count(params['forecast16_count'])
+
         except KeyError as e:
+            print(str(e))
             raise Exception(repr('Not a valid config key: ' + str(e)))
 
         return None
@@ -53,31 +59,46 @@ class WeatherRequest(object):
         self.__lang = lang
         return None
 
+    def set_forecast16_count(count):
+        """ Set the day count for forecast 16 request """
+        self.__forecast16_count = count
+        return None
+
     def get_weather(self):
         """ Send the request """
-
         payload = {
             'id': self.__location,
             'units': self.__units,
             'lang': self.__lang,
             'APPID': self.__api_key
         }
-        
+
+        # Two types of 'forecast' requests are available
+        if self.__request_type == 'forecast5':
+            self.set_request_type('forecast')
+
+        elif self.__request_type == 'forecast16':
+            self.set_request_type('forecast/daily')
+            payload['cnt'] = self.__forecast16_count
+
         try:
             response = get(
                 self.__endpoint_base_url + self.__request_type,
                 params=payload
             ).json()
-        
+
         except ValueError as e:
             raise Exception(e)
-        
+
         if self.__request_type == 'weather':
             response = self.__validate_response_weather(response)
-        
+
         elif self.__request_type == 'forecast':
             response = self.validate_response_forecast(response)
-            
+
+        elif self.__request_type == 'forecast/daily':
+            response = self.validate_response_forecast(response)
+
         return response
 
     def validate_response_weather(self, response):
