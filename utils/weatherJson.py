073ff53218ed73715242d956b88f8506286050f3
falconsmilie <shane@rededitor.net>
2017-01-05 20:55:12 +0100

    Extracted JSON string functions to own class

---
+++ utils/weatherJson.py
@@ -0,0 +1,16 @@
+import json
+
+
+class WeatherJson(object):
+
+    def fix_json_string(self, json_string):
+        """ Response from server is a dict and cache is a string """
+
+        if type(json_string) is dict:
+            json_string = json.dumps(json_string)
+
+        weather = json_string.replace("'",'"')
+        weather = weather.replace('u"','"')
+        weather = json.loads(weather)
+
+        return weather
