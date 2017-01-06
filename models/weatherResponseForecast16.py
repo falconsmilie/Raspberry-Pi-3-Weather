d0049baef32651cf8cca316eac9a7e8e35033fab
falconsmilie <shane@rededitor.net>
2017-01-07 00:55:41 +0100

    Added calls to setters for base members

--- models/weatherResponseForecast16.py
+++ models/weatherResponseForecast16.py
@@ -14,11 +14,19 @@ class WeatherResponseForecast16(WeatherResponse):
     def set_response(self, weather):
         """ Sets response variables to local members """
 
+        print(weather)
+        exit()
+
         try:
             json_util = WeatherJson()
             weather = json_util.fix_json_string(weather)
 
-            self.set_city_name(weather['name'])
+            # Base Members
+            self.set_city_name(weather['city']['name'])
+            self.set_city_id(weather['city']['id'])
+            self.set_country(weather['city']['country'])
+            self.set_coord_lat(weather['city']['coord']['lat'])
+            self.set_coord_lon(weather['city']['coord']['lon'])
 
             # TODO: Add other members
 
