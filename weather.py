4c096388b19df42f8b25fb5a56f0c0ebc2d512f7
falconsmilie <shane@rededitor.net>
2017-01-07 12:35:26 +0100

    Updated controller test for new conditions class

--- weather.py
+++ weather.py
@@ -48,11 +48,12 @@ try:
     weatherlist = response.get_list()
 
     # Test forecast
-    for w_list in weatherlist:
-        print(w_list.get_temp())
+    #for w_list in weatherlist:
+        #print(w_list.get_temp())
 
     # Test weather list
-    #print(weatherlist.get_temp())
+    print(weatherlist.get_temp())
+    print(weatherlist.get_conditions().get_wind_speed())
 
 except Exception as e:
     print(e)
