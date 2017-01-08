1eff2367122d5150f1b422dd49554b7adf112cfb
falconsmilie <shane@rededitor.net>
2017-01-08 15:04:51 +0100

    Added key checking validation method for setting/getting config

--- models/weatherConfig.py
+++ models/weatherConfig.py
@@ -7,8 +7,13 @@ class WeatherConfig(object):
 
     def __init__(self):
         """ Set local members """
+
         self.__path_to_config_file = 'config/weatherConfig.json'
         self.__config = None
+        self.__config_keys = [
+            'api_key', 'location', 'request_type', 'forecast_count_5',
+            'forecast_count_16', 'units', 'lang'
+        ]
 
     def set_config(self):
         """ Read and then set JSON config file to local member """
@@ -24,6 +29,8 @@ class WeatherConfig(object):
             self.__config = json.load(json_config)
             json_config.close()
 
+            self.validate_config()
+
         else:
             raise Exception('Cannot load Weather Config.')
 
@@ -37,4 +44,15 @@ class WeatherConfig(object):
                 'There is nothing in the Weather Config to load.'
             )
 
-        return self.__config
+        if self.validate_config():
+            return self.__config
+
+    def validate_config(self):
+        """ Check keys are all there. Pain to update, but cool for user
+        of this class knowing they are going to get all the keys back.
+        """
+        for key in self.__config_keys:
+            if not key in self.__config:
+                raise Exception('Data is missing from Config: ' + key)
+
+        return True
