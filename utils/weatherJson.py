c84d775f96d8c03736add843c3bcc7f7b49e0a07
falconsmilie <shane@rededitor.net>
2017-01-15 00:54:39 +0100

    PEP8 update

--- utils/weatherJson.py
+++ utils/weatherJson.py
@@ -6,7 +6,7 @@ class WeatherJson(object):
     def fix_json_string(self, json_string):
         """ Response from server is a dict and cache is a string """
 
-        if type(json_string) is dict:
+        if isinstance(json_string, dict):
             json_string = json.dumps(json_string)
 
         weather = json_string.replace("'", '"')
